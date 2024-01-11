0bcb95b77a5c131e6e3da7cd0bebe15c98309802
Aiham00 <91168544+Aiham00@users.noreply.github.com>
2024-01-11 18:40:03 +0100

    cleaned front end code

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # MoviesRecommender
-pip install Flask
+Back-end istallation
+pip install Flask 
 pip install aiosqlite
-pip install pyjwt
-pip install lenskit
+pip install pyjwt 
+pip install lenskit // does not work on the last virsion of Python 3.12
 pip install -U flask-cors
 
 run python main.py
 or python3 main.py
 
+Front-end istallation
+run 
+npm install
+Node spa/app.js
+
 send requests to http://127.0.0.1:5000
-ex: http://127.0.0.1:5000/recommendation?userId1=2&userId2=3&type=false
+ex:post request to http://127.0.0.1:5000/recommendation?userId1=2&userId2=3&type=false
+
+Note: that you need Node.Js installed to run the front-end application. 
--- spa/public/js/recommendation.js
+++ spa/public/js/recommendation.js
@@ -42,11 +42,6 @@ async function loadRecommenationsPage(query) {
         tdh2.innerText = "Genres"
         trh.appendChild(tdh2)
 
-        /*const tdh3 = document.createElement('td')
-        tdh3.innerText = "End date"
-        trh.appendChild(tdh3)*/
-
-
         const tbody = document.createElement('tbody')
         table.appendChild(tbody)
 
@@ -71,10 +66,6 @@ async function loadRecommenationsPage(query) {
           td2.innerText = movie[0]
           tr.appendChild(td2)
 
-         /* const td3 = document.createElement('td')
-          td3.innerText = movie.endDate
-          tr.appendChild(td3)*/
-
           const td4 = document.createElement("td")
           tr.appendChild(td4)
 
@@ -83,45 +74,7 @@ async function loadRecommenationsPage(query) {
 
           const subTd1 = document.createElement('td')
           subTr1.appendChild(subTd1)
-          
-          /*const eventAnchor = document.createElement('a')
-          eventAnchor.innerText = "See Event"
-          const url = "/events/"+movie.eventId
-          handleAnchorOnClick(eventAnchor,url)
-          subTd1.appendChild(eventAnchor)
 
-          if(isOrganizationLoggedin()){
-            const subTd2 = document.createElement('td')
-            subTr1.appendChild(subTd2) 
-  
-            const appAnchor = document.createElement('a')
-            appAnchor.innerText = "See applications"
-            const appUrl = "/applications/"+movie.eventId
-            handleAnchorOnClick(appAnchor,appUrl)
-            subTd2.appendChild(appAnchor)
-  
-            const subTr2 = document.createElement("tr")
-            td4.appendChild(subTr2)
-  
-            const subTd3 = document.createElement('td')
-            subTr2.appendChild(subTd3)
-  
-            const UpdateEventAnchor = document.createElement('a')
-            UpdateEventAnchor.innerText = "Update Event"
-            const UpdateEventUrl = "/events/update/"+movie.eventId
-            handleAnchorOnClick(UpdateEventAnchor,UpdateEventUrl)
-            subTd3.appendChild(UpdateEventAnchor)
-  
-            const subTd4 = document.createElement('td')
-            subTr2.appendChild(subTd4)        
-  
-            const deleteEventAnchor = document.createElement('a')
-            deleteEventAnchor.innerText = "Delete Event"
-            const deleteEventUrl = "/events/delete/"+movie.eventId
-            handleAnchorOnClick(deleteEventAnchor,deleteEventUrl)
-            subTd4.appendChild(deleteEventAnchor)
-
-          }*/
         }
       })
       
